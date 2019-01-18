5cdce86eac01bc3637dc7fc27859a5f0ac20fc25
Zampieri Giacomo <giacomo.zampieri@barsanti.edu.it>
2019-01-18 09:34:25 +0100

    18/1/2019 - 9:30 : Riscrittura dei metodi precedentemente implementati in modo da adattare il programma ad un input dalla console seriale, completati l'inserimento della variabile di riferimento per la vittoria e il metodo per la gestione dei turni, inserimento e verifica dei valori da sommare ogni turno, inizio scrittura del metodo per la verifica delle condizioni di vittoria.

--- angelaGame/angelaGame.ino
+++ angelaGame/angelaGame.ino
@@ -1,45 +1,71 @@
 
   //BOTTONI
-  int buttonPin1 = 10;
-  int buttonPin2 = 11;
-  int buttonPin3 = 12;
 
   //Variabili
-  int n = 50;
+  int nVincente = 0;
+  int sommaDadiTotale = 0;
+  int nGiocatoPrima = 0;
   int stato = 0;
+  int turno = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(buttonPin1, INPUT_PULLUP);
-  pinMode(buttonPin2, INPUT_PULLUP);
-  pinMode(buttonPin3, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(stato == 0)//Inserimento della variabile da raggiungere per vincere
+  {
+    primoValore();//Controllo della variabile inquanto deve rispettare determinate caratteristiche
+  }
+  else if(stato = 1)//Inizio della partita vera e propria
+  {
+    giocata();
+  }
+  
   
 }
 
-
-void impostaPrimoValore()
+void primoValore()//Metodo di Controllo della variabile da raggiungere
 {
-  if(stato == 0)
+  if(Serial.available())
   {
-    if (digitalRead(buttonPin1) == LOW)
+    Serial.println("Inserisci un mumero compreso tra 30 e 99");
+    int s = Serial.read();
+    if(s < 30)
     {
-      if(n >= 30)
-      {
-        n--;
-      }
+      Serial.println("il numero è troppo piccolo");
     }
-
-    if(digitalRead(buttonPin3) == LOW)
+    else if(s > 100)
     {
-      if(n < 100)
-      {
-        n++;
-      }
+      Serial.println("il numero è troppo grande");
+    }
+    else
+    {
+      nVincente = s;
+      stato = 1;
     }
   }
+}
+
+
+void giocata()//metodo che gestisce, a turno, l'inserimento del valore dei dadi e li somma 
+{
+  if(Serial.available())
+  {
+    Serial.println("Inserisci un mumero compreso tra 1 e 6");
+    int d = Serial.read();
+    //Se è la prima giocata si può inserire il valore 0 e quindi passare il turno
+    nGiocatoPrima == 0 ? (sommaDadiTotale = sommaDadiTotale + d, nGiocatoPrima = 7, turno = turno == 0 ? 1 : 0) :
+    (d < 1 ? Serial.println("il numero è troppo piccolo") : (d > 6 ? Serial.println("il numero è troppo grande") : //Se non è la prima giocata controlla prima se è compreso tra 1 e 6
+    (d == 1 && nGiocatoPrima == 6 || d == 6 && nGiocatoPrima == 1 || //Se è compreso tra 1 e 6 controlla se il numero rispecchia le condizioni adatte
+    d == 2 && nGiocatoPrima == 5 || d == 5 && nGiocatoPrima == 2 || 
+    d == 3 && nGiocatoPrima == 4 || d == 4 && nGiocatoPrima == 3)
+    ? Serial.println("non puoi usare questo valore") : (sommaDadiTotale = sommaDadiTotale + d, nGiocatoPrima = d, turno = turno == 0 ? 1 : 0)));
+  }
+}
+
+void vittoriaSconfitta()
+{
+  
 }
