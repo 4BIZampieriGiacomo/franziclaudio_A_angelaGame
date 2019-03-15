851d4c245d501dc02ca72631a543851adcca32c9
Zampieri Giacomo <giacomo.zampieri@barsanti.edu.it>
2019-03-15 08:13:20 +0100

    8/3/2019 : Completamento metodi di elaborazione dei dati, in corso l'implementazione dei messaggi tramite lcd

--- angelaGame/angelaGame.ino
+++ angelaGame/angelaGame.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h>
 //LCD
-LiquidCrystal lcd(12,11,10,9,8,7);
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 //BOTTONI
 int b1;//diminuisce
@@ -16,16 +16,16 @@ int nGiocatoPrima;//back-up della giocata precedente
 bool primoTurno;
 int stato;//Indica i vari momenti del gioco
 int turno;// indica i turni dei due giocatori: 0 = Giocatore 1 , 1 = Giocatore 2
+bool winLose;
 
 
 void setup() {
-  if(primaAperturaProgramma)
+  if (primaAperturaProgramma)
   {
-    Serial.begin(9600);
-    lcd.begin(0,0);
-    pinMode(b1,INPUT_PULLUP);
-    pinMode(b2,INPUT_PULLUP);
-    pinMode(b3,INPUT_PULLUP);
+    lcd.begin(16, 2);
+    pinMode(b1, INPUT_PULLUP);
+    pinMode(b2, INPUT_PULLUP);
+    pinMode(b3, INPUT_PULLUP);
     primaAperturaProgramma = false;
   }
   nVincente = 0;
@@ -42,24 +42,39 @@ void loop() {
   {
     if (x == 0)
     {
-      Serial.println("Inserisci un mumero compreso tra 30 e 99 per fissare la meta da raggiungere");
-      x = 1;
+      lcd.setCursor(0, 0);
+      lcd.print("Inserisci numero");
+      lcd.setCursor(0, 1);
+      lcd.print("tra 30 e 99: ");
     }
-    
-    nVincente = selectValue(1);
-    //primoValore();//Controllo della variabile inquanto deve rispettare determinate caratteristiche
+    nVincente = selectValue(1);//Imposto la meta
+    if (nVincente != 0)
+    {
+      lcd.setCursor(13, 1);
+      lcd.print(nVincente);
+      delay(1500);
+    }
   }
   else if (stato == 1) //Inizio della partita vera e propria
   {
-    Serial.println(turno == 0 ? "Turno del giocatore 1" : "Turno del giocatore 2");//Indica il turno in corso
-    Serial.println("Inserisci il numero da giocare compreso tra 1 e 6");
-    //giocata();//Metodo che gestisce i turni e il controllo dei vari inserimenti
-    playerTurn();
-    vittoriaSconfitta();//Metodo che controlla ogni turno se si ha raggiunto le condizioni di fine partita e decreta il vincitore/perdente
+    lcd.clear();
+    lcd.setCursor(4, 0);
+    lcd.print(turno == 0 ? "Player 1" : "Player 2");//Indica il turno in corso
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.println("Inserisci numero");
+    lcd.print("tra 1 e 6: ");
+    playerTurn();//Metodo che gestisce i turni
+    lcd.clear();
+    Serial.println("N° giocato: " + String(nGiocatoPrima));
+    Serial.println("Totale: " + String(sommaDadiTotale));
+    if (vittoriaSconfitta() != "") //Metodo che controlla ogni turno se si ha raggiunto le condizioni di fine partita e decreta il vincitore/perdente
+    {
+      Serial.println("Il giocatore" + vittoriaSconfitta() + (winLose ? "ha vinto!!!" : "ha perso!!!"));
+    }
   }
-  else if (stato == 3) //partita finita, resetto le variabili
-  {
-    setup();
+  else if (stato == 3) {
+    setup();  //partita finita, reset delle variabili
   }
 }
 
@@ -67,177 +82,92 @@ void loop() {
 //metodo che permette in base alla fase del gioco di impostare la meta o il numero del dado giocato
 int selectValue(int condiz)
 {
+  bool finish = false;
   int value;
-  bool c1 = value <= 99 && value >= 30;//condizione da rispettare per impostare la meta
-  bool c2 = value != (7 - nGiocatoPrima) && value < 7 && value >= 0;
-  bool c3 = value != nGiocatoPrima && value != (7 - nGiocatoPrima) && value < 7 && value > 0;//condizione da rispettare per l'inserimento del numero del dado
-  
+  bool case1 = value <= 99 && value >= 30;//condizione da rispettare per impostare la meta
+  bool case2 = value != (7 - nGiocatoPrima) && value < 7 && value >= 0;//condizione da rispettare per l'inserimento del numero del dado se è il primo turno
+  bool case3 = value != nGiocatoPrima && value != (7 - nGiocatoPrima) && value < 7 && value > 0;//condizione da rispettare per l'inserimento del numero del dado
+
   //assegnazione valore base
-  if(condiz == 1)
-  {
+  if (condiz == 1) {
     value = 30;
   }
-  if(condiz = 2)
-  {
+  if (condiz == 2) {
     value = 0;
   }
-  if(condiz = 3)
-  {
+  if (condiz == 3) {
     value = 1;
   }
+  lcd.setCursor(13, 1);
+  lcd.print(value);
 
-  
-  //diminuisce
-  if(b1 == LOW)
+  while (finish)
   {
-    if(condiz == 1)
+    //diminuisce
+    if (b1 == LOW)
     {
-      value--;
-      if(!c1)
-      {
-        value++;
-      }
-    }
-    if(condiz == 2)
-    {
-      value--;
-      if(!c2)
-      {
-        value++;
-      }
-    }
-    if(condiz == 3)
-    {
-      value--;
-      if(!c3)
-      {
-        value++;
-      }
-    }
-  }
-
-  //conferma
-  if(b2 == LOW)
-  {
-    return value;
-  }
-
-  //aumenta
-  if(b3 == LOW)
-  {
-     if(condiz == 1)
-    {
-      value++;
-      if(!c1)
+      if (condiz == 1)
       {
         value--;
+        if (!case1) {
+          value++;
+        }
       }
-    }
-    if(condiz == 2)
-    {
-      value++;
-      if(!c2)
+      if (condiz == 2)
       {
         value--;
+        if (!case2) {
+          value++;
+        }
       }
+      if (condiz == 3)
+      {
+        value--;
+        if (!case3) {
+          value++;
+        }
+      }
+      lcd.setCursor(13, 1);
+      lcd.print(value);
     }
-    if(condiz == 3)
+
+    //conferma
+    if (b2 == LOW) {
+      finish = true;
+      return value;
+    }
+
+    //aumenta
+    if (b3 == LOW)
     {
-      value--;
-      if(!c3)
+      if (condiz == 1)
       {
         value++;
+        if (!case1) {
+          value--;
+        }
       }
+      if (condiz == 2)
+      {
+        value++;
+        if (!case2) {
+          value--;
+        }
+      }
+      if (condiz == 3)
+      {
+        value++;
+        if (!case3) {
+          value--;
+        }
+      }
+      lcd.setCursor(13, 1);
+      lcd.print(value);
     }
   }
 }
 
-//void primoValore()//Metodo di Controllo della variabile da raggiungere
-//{
-  //while (Serial.available() == 0) {}
-  //int s = Serial.parseInt();
-  //if (s < 30)
-  //{
-    //Serial.println("il numero: " + String(s) + " è troppo piccolo");
-  //}
-  //if (s > 99)
-  //{
-    //Serial.println("il numero: " + String(s) + " è troppo grande");
-  //}
-  //if (s >= 30 && s <= 99)
-  //{
-    //Serial.println("il numero: " + String(s) + " è stato inserito correttamente");
-    //nVincente = s;
-    //stato = 1;
-  //}
-  //x = 0;
-//}
-
-
-//void giocata()//metodo che gestisce, a turno, l'inserimento del valore dei dadi e li somma
-//{
-  //while (Serial.available() == 0) {}
-  //int d = Serial.parseInt();
-  //if (primoTurno == true)
-  //{
-    //if (d < 0)
-    //{
-      //Serial.println("il numero: " + String(d) + " è troppo piccolo");
-    //}
-    //else
-    //{
-      //if (d > 6)
-      //{
-        //Serial.println("il numero: " + String(d) + " è troppo grande");
-      //}
-      //else
-      //{
-        //sommaDadiTotale = sommaDadiTotale + d;
-        //d == 0 ? nGiocatoPrima = 7 : nGiocatoPrima = d;
-        //primoTurno = false;
-        //turno = turno == 0 ? 1 : 0;
-        //Serial.println("numero raggiunto: " + String(sommaDadiTotale));
-      //}
-    //}
-  //}
-  //else
-  //{
-    //if (d < 1)
-    //{
-      //Serial.println("il numero: " + String(d) + " è troppo piccolo");
-    //}
-    //else
-    //{
-      //if (d > 6)
-      //{
-        //Serial.println("il numero: " + String(d) + " è troppo grande");
-      //}
-      //else
-      //{
-        //if (d == nGiocatoPrima)
-        //{
-          //Serial.println("il numero inserito: " + String(d) + " è già stato utilizzato il turno precedente");
-        //}
-        //else
-        //{
-          //if (d == 1 && nGiocatoPrima == 6 || d == 6 && nGiocatoPrima == 1 || d == 2 && nGiocatoPrima == 5 || d == 5 && nGiocatoPrima == 2 || d == 3 && nGiocatoPrima == 4 || d == 4 && nGiocatoPrima == 3)
-          //{
-            //Serial.println("il numero inserito: " + String(d) + "non può essere utilizzato");
-          //}
-          //else
-          //{
-            //sommaDadiTotale = sommaDadiTotale + d;
-            //nGiocatoPrima = d;
-            //turno = turno == 0 ? 1 : 0;
-            //Serial.println("numero raggiunto: " + String(sommaDadiTotale));
-          //}
-        //}
-      //}
-    //}
-  //}
-//}
-
-
+//Metodo che dopo l'inserimento del numero del dado effettua la somma totale e cambia il turno
 void playerTurn()
 {
   if (primoTurno == true)
@@ -247,7 +177,6 @@ void playerTurn()
     nGiocatoPrima = n;
     primoTurno = false;
     turno = turno == 0 ? 1 : 0;
-    //Serial.println("numero raggiunto: " + String(sommaDadiTotale));
   }
   else
   {
@@ -255,38 +184,42 @@ void playerTurn()
     sommaDadiTotale = sommaDadiTotale + n;
     nGiocatoPrima = n;
     turno = turno == 0 ? 1 : 0;
-    //Serial.println("numero raggiunto: " + String(sommaDadiTotale));
   }
 }
 
-
-void vittoriaSconfitta()//metodo che definisce chi ha vinto e chi ha perso una volta raggiunte determinate condizioni
+//metodo che definisce chi ha vinto e chi ha perso una volta raggiunte determinate condizioni
+String vittoriaSconfitta()
 {
   if (nVincente == sommaDadiTotale)
   {
     if (turno == 0)
     {
-      Serial.println("Il giocatore 2 ha vinto!!!");
-      stato = 3;
+      winLose = true;
+      return 2;
     }
     else
     {
-      Serial.println("Il giocatore 1 ha vinto!!!");
-      stato = 3;
+      winLose = true;
+      return 1;
     }
+    stato = 3;
   }
-
-  if (nVincente < sommaDadiTotale)
+  else if (nVincente < sommaDadiTotale)
   {
     if (turno == 0)
     {
-      Serial.println("Il giocatore 2 ha perso!!!");
-      stato = 3;
+      winLose = false;
+      return 2;
     }
     else
     {
-      Serial.println("Il giocatore 1 ha perso!!!");
-      stato = 3;
+      winLose = false;
+      return 1;
     }
+    stato = 3;
+  }
+  else
+  {
+    return "";
   }
 }
